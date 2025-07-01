6a03a4cdc6ff8ec4ba784521291c907547550d76
cris777-V <cristhianmosquera723@gmail.com>
2025-06-30 19:02:14 -0500

    Add server improvements and gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('🟢 MongoDB conectado'))
-.catch((err) => console.error('🔴 Error en MongoDB:', err));
-
-
 const express = require('express');
+
 const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('🟢 MongoDB conectado'))
+  .catch((err) => console.error('🔴 Error en MongoDB:', err));
 
 app.get('/', (req, res) => {
-    res.send('Servidor GestionMax funcionando');
+  res.send('Servidor GestionMax funcionando');
 });
 
-app.listen(3000, () => {
-    console.log('Servidor escuchando en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
